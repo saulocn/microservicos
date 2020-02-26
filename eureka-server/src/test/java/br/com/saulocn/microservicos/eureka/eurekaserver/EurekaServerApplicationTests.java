a555d79403d97e0edc8ae1fd3b2603443cc91a76
Saulo de Carvalho Neto <sacarvalho@uolinc.com>
2020-02-26 13:36:14 -0300

    Registrando fornecedor no Eureka

---
+++ eureka-server/src/test/java/br/com/saulocn/microservicos/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saulocn.microservicos.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
